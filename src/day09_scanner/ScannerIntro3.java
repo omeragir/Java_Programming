e93a73ead0879bb0df2fee7c04d528d70f37359d
omeragir <omeragir08@gmail.com>
2023-02-27 14:26:16 +0100

    ScannerIntro3

---
+++ src/day09_scanner/ScannerIntro3.java
@@ -0,0 +1,19 @@
+package day09_scanner;
+
+import java.util.Scanner;
+
+public class ScannerIntro3 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+
+       System.out.println("Enter a char : ");
+       char ch=input.next().charAt(0);
+        System.out.println("Your entered "+ch);
+
+        System.out.println("Would you like to continue : ");
+        String answer=input.next();
+       // String answer=input.nextLine();
+        System.out.println("You have entered "+answer);
+
+    }
+}
