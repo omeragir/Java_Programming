3f83b9b611b1e44060828a581d26e3004f3d85bb
omeragir <omeragir08@gmail.com>
2023-03-01 12:26:28 +0100

    Login

---
+++ src/day11_string_continue/DigitLetterSpecialChar.java
@@ -0,0 +1,25 @@
+package day11_string_continue;
+
+import java.util.Scanner;
+
+public class DigitLetterSpecialChar {
+    public static void main(String[] args) {
+
+
+        Scanner input=new Scanner(System.in);
+
+
+
+
+    }
+}
+/*
+Create a class named DigitLetterSpecialChar and wirte a program that can ask the user to enter a word
+	        - if the word starts with digits, print "first character is digit"
+	        - if the word starts with uppercase letters, print "first character is lowercase letter"
+	        - if the word starts with lowercase letters, print "first character is uppercase letter"
+	        - if the word starts with special characters, print "first character is special character"
+
+
+        	HINT: You need to check the ascii table
+ */
---
+++ src/day11_string_continue/Login.java
@@ -0,0 +1,40 @@
+package day11_string_continue;
+
+import java.util.Scanner;
+
+public class Login {
+    public static void main(String[] args) {
+
+
+        String correctUserName = "Cydeo";
+        String correctPassWord = "Cydeo";
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your user name : ");
+        String username = input.nextLine();
+
+        System.out.println("Enter your password : ");
+        String password = input.nextLine();
+        input.close();
+
+        if (username.equals(correctPassWord) && password.equals(correctPassWord)) {
+            System.out.println("You are now logged in");
+
+        } else {
+            System.err.println("Incorrect username or password. Please try again");
+        }
+
+
+    }
+}
+/*
+Create a class named LogIn
+			2.1 Ask the user to enter the username & password
+			2.2 print "You are now logged in" If user entered valid username and password
+				otherwise print the error message "Incorrect username or password. Please try again"
+
+			Note: Assume that the valid credentials are:
+						username: Cydeo
+						password: WoodenSpoon
+ */
