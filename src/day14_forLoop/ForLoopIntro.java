ea9c93a88a1c88238047bd222982f5df69969004
omeragir <omeragir08@gmail.com>
2023-03-08 11:14:31 +0100

    ForLoopIntro

---
+++ src/day14_forLoop/ForLoopIntro.java
@@ -0,0 +1,19 @@
+package day14_forLoop;
+
+public class ForLoopIntro {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
+
+
+        System.out.println("--------------------------------------------");
+        for(int i=1;i<=5;i++) {
+            System.out.println("Hello World");
+        }
+    }
+}
