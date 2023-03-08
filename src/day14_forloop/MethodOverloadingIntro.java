7406e32307a057c122cbfe85ccb1fe78713cadc7
omeragir <omeragir08@gmail.com>
2023-03-08 09:35:08 +0100

    MethodOverloadingIntro

---
+++ src/day14_forloop/MethodOverloadingIntro.java
@@ -0,0 +1,16 @@
+package day14_forloop;
+
+public class MethodOverloadingIntro {
+    public static void main(String[] args) {
+        methodA(5,6);
+
+    }
+    public static void methodA(){}
+
+    public static void methodA(int a){}
+
+    public static void methodA(double a,int b){}
+
+
+
+}
