17af733a698be2be29b0d630f6feb3633bf9ca75
omeragir <omeragir08@gmail.com>
2023-03-08 09:05:21 +0100

    WarmUpTask

---
+++ src/day14_loop/WarmUpTask.java
@@ -0,0 +1,38 @@
+package day14_loop;
+
+public class WarmUpTask {
+    public static void main(String[] args) {
+
+    }
+
+    public static int sumOf2IntegerNumbers(int n1, int n2) {
+        return n1 + n2;
+
+
+    }
+
+    public static int sumOf3IntegerNumber(int n1, int n2, int n3) {
+        return sumOf2IntegerNumbers(n1, n2) + n3;
+        //  return n1+n2+n3;
+    }
+
+    public static int sumOf4IntegerNumber(int n1, int n2, int n3, int n4) {
+        //return n1+n2+n3+n4;
+        // return sumOf3IntegerNumber(n1,n2,n3)+n4;
+        //return sumOf2IntegerNumbers(n1, n2) + n3 + n4;
+        return sumOf2IntegerNumbers(sumOf3IntegerNumber(n1, n2, n3), n4);
+
+
+    }
+
+}
+/*
+  1. create a method that can find the sum of two integer numbers
+	                method name: sumOf2Numbers
+
+	    2. create a method that can find the sum of three integer numbers
+	                method name: sumOf3Numbers
+
+	    3. create a method that can find the sum of four integer numbers
+	                method name: sumOf4Numbers
+ */
