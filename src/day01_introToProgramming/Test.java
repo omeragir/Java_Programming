ea111423c76f75346a7b2419e1fdc363c92dc3e9
omeragir <omeragir08@gmail.com>
2023-03-08 15:42:08 +0100

    StringUtilityTest

---
+++ src/day01_introToProgramming/Test.java
@@ -0,0 +1,13 @@
+package day01_introToProgramming;
+
+import utilities.StringUtility;
+
+public class Test {
+    public static void main(String[] args) {
+        String word="Wooden Spoon";
+
+        String result=StringUtility.reverse(word);
+        System.out.println(result);
+    }
+
+}
