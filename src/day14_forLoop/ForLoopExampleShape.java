74f9e335239e54911207acf333c8215eb07f895e
omeragir <omeragir08@gmail.com>
2023-03-09 11:21:05 +0100

    ForLoopExampleModule

--- src/day14_forLoop/ForLoopExampleShape.java
+++ src/day14_forLoop/ForLoopExampleShape.java
@@ -3,11 +3,12 @@ package day14_forLoop;
 public class ForLoopExampleShape {
     public static void main(String[] args) {
 
+        int rows = 8;
+        int columns = 6;
 
+        for (int i = 1; i <= rows; i++) {
 
-        for (int i = 1; i <= 8; i++) {
-
-            for (int j = 1; j <= 6; j++) {
+            for (int j = 1; j <= columns; j++) {
                 System.out.print("*");
 
 
---
+++ src/day14_forLoop/ForLoopModule.java
@@ -0,0 +1,27 @@
+package day14_forLoop;
+
+import java.util.Scanner;
+
+public class ForLoopModule {
+    public static void main(String[] args) {
+
+
+
+        for(int i=1;i<=100;i++){
+            if(i%3==0){
+                System.out.print("FIN ");
+            } else if (i%3==0&&i%5==0) {
+                System.out.print("FINRA ");
+
+            }else if (i%5==0) {
+                System.out.print("RA ");
+
+            }
+            else {
+                System.out.print(i+" ");
+            }
+        }
+
+
+    }
+}
