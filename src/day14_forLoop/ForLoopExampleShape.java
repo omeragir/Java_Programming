5d67e97ab07db7f48f0c6e63262f164d9b67b3cb
omeragir <omeragir08@gmail.com>
2023-03-09 11:08:38 +0100

    ForLoopExampleShape

---
+++ src/day14_forLoop/ForLoopExampleShape.java
@@ -0,0 +1,21 @@
+package day14_forLoop;
+
+public class ForLoopExampleShape {
+    public static void main(String[] args) {
+
+
+
+        for (int i = 1; i <= 8; i++) {
+
+            for (int j = 1; j <= 6; j++) {
+                System.out.print("*");
+
+
+            }
+            System.out.println();
+
+        }
+
+
+    }
+}
