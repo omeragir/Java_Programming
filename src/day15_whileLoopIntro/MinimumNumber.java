93009a65ac00a5ba647f41ecdd7a8516e7577dbc
omeragir <omeragir08@gmail.com>
2023-03-13 09:29:03 +0100

    MinNumber

--- src/day15_whileLoopIntro/MinimumNumber.java
+++ src/day15_whileLoopIntro/MinimumNumber.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class MinimumNumber {
 
     public static void main(String[] args) {
-        int min = 0;
+        int min = 2147483647;
 
         for (int i = 0; i < 5; i++) {
             System.out.println("Enter a number : ");
