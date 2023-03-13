23f3499778f4cbcb85bf03db34d422e37ce4c57c
omeragir <omeragir08@gmail.com>
2023-03-13 11:10:23 +0100

    Continue Statement

---
+++ src/day15_whileLoopIntro/ContinueStatement.java
@@ -0,0 +1,36 @@
+package day15_whileLoopIntro;
+
+public class ContinueStatement {
+    public static void main(String[] args) {
+        for (int i = 0; i < 6; i++) {
+
+            if (i == 4) {
+                continue;
+            }
+            System.out.println(i);
+        }
+        System.out.println("-----------------------------");
+
+        for (int i = 10; i < 20; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            System.out.println(i);
+        }
+        System.out.println("--------------------------");
+
+        for (char i = 'A'; i <= 'G'; i++) {
+            if (i == 'B' || i == 'E') {
+                continue;
+            }
+            System.out.println(i);
+        }
+        System.out.println("--------------------------");
+
+
+
+
+    }
+
+
+}
