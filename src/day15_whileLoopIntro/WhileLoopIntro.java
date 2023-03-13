98793a1d2a7671a738e98f61c0c4219fd4831617
omeragir <omeragir08@gmail.com>
2023-03-13 14:06:09 +0100

    WhileLoopPractice

---
+++ src/day15_whileLoopIntro/WhileLoopIntro.java
@@ -0,0 +1,15 @@
+package day15_whileLoopIntro;
+
+public class WhileLoopIntro {
+    public static void main(String[] args) {
+        if(true){
+            System.out.println("Hello world");
+
+        }
+        System.out.println("-----------------------------------");
+        while (true){
+            System.out.println("Hello Cydeo");
+        }
+
+    }
+}
---
+++ src/day15_whileLoopIntro/WhileLoopPractice.java
@@ -0,0 +1,27 @@
+package day15_whileLoopIntro;
+
+import java.util.Scanner;
+
+public class WhileLoopPractice {
+    public static void main(String[] args) {
+
+     Scanner input=new Scanner(System.in);
+
+        System.out.println("Enter your age: ");
+        int age=input.nextInt();
+
+        while (!(age>=1)||(age>150)){
+            System.out.println("Invalid entry! Please re-enter your age");
+            age=input.nextInt();
+        }
+
+
+        if(age>21){
+            System.out.println("Eligible");
+        }else {
+            System.out.println("Not Eligible");
+        }
+
+
+    }
+}
