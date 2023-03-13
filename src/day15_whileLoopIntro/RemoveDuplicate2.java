fe0134bd64e3cdd5c5ca07ff21ad12db0670e9a0
omeragir <omeragir08@gmail.com>
2023-03-13 11:16:51 +0100

    RemoveDuplicate2

---
+++ src/day15_whileLoopIntro/RemoveDuplicate2.java
@@ -0,0 +1,30 @@
+package day15_whileLoopIntro;
+
+public class RemoveDuplicate2 {
+    public static void main(String[] args) {
+
+        String str = "xyzxyzxyz";
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+
+            if (result.contains("" + str.charAt(i)))
+
+                continue;
+
+            result += str.charAt(i);
+        }
+
+        System.out.println(result);
+
+    }
+}
+/*
+Write a program that can remove the duplicated characters from the a string
+
+				Ex:
+					str = "aabbbcccc"
+
+					output:
+						abc
+ */
