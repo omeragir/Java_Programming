f26fd3ec25c69d51069d2a85efe4b2719a419b57
omeragir <omeragir08@gmail.com>
2023-03-14 07:21:37 +0100

    LogInLoop

---
+++ src/day16_nestedLoop/CydeoLogIn.java
@@ -0,0 +1,59 @@
+package day16_nestedLoop;
+
+import java.util.Scanner;
+
+public class CydeoLogIn {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your user name : ");
+        String userName = input.next();
+
+        System.out.println("Enter your password : ");
+        String passWord = input.next();
+
+        if (userName.equals("Cydeo") && passWord.equals("WoodenSpoon")) {
+            System.out.println("Logged in");
+        } else {
+            for (int i = 0; i <= 2; i++) {
+                System.err.println("Invalid user name or password,please re-enter");
+                System.out.println("Enter your user name : ");
+                userName = input.next();
+
+                System.out.println("Enter your password : ");
+                passWord = input.next();
+
+                if (userName.equals("Cydeo") && passWord.equals("WoodenSpoon")) {
+                    System.out.println("Logged in");
+                    break;
+
+                }
+
+
+            }
+
+
+        }
+
+        if (!(userName.equals("Cydeo") && passWord.equals("WoodenSpoon"))) {
+            System.out.println("Your account now is locked,please contact with support team");
+        }
+        input.close();
+    }
+
+
+}
+/*
+1. Create a class named CydeoLogIn. you are writing a code for the log-in function of the Cydeo Application, assume that your credentials are:
+                    username: Cydeo
+                    password: WoodenSpoon
+ Ask the user to enter their credentials.
+                If credentials are matched, your program should print "Logged in."
+
+                If the credentials are not matched, the program should allow
+                the user to have three attempts to enter correct credentials
+
+                if all three attempts are failed, then print "Your account is lucked."
+
+
+ */
