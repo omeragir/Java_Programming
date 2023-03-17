58b3d100004f9489c18dc5bcc6f3d8ce927522cd
omeragir <omeragir08@gmail.com>
2023-03-17 09:42:47 +0100

    FrequencyOfCharacters

--- src/day17_customClass/FrequencyOfCharacters.java
+++ src/day17_customClass/FrequencyOfCharacters.java
@@ -2,23 +2,23 @@ package day17_customClass;
 
 public class FrequencyOfCharacters {
     public static void main(String[] args) {
-        String str="aabcccd";
-        String result="";
+        String str = "aabcccd";
+        String result = "";
 
 
         for (int j = 0; j < str.length(); j++) {
-            char ch=str.charAt(j);
-            int count=0;
+            char ch = str.charAt(j);
+            int count = 0;
 
             for (int i = 0; i < str.length(); i++) {
-                if(str.charAt(i)==ch){
+                if (str.charAt(i) == ch) {
                     count++;
                 }
-                }
-            if(result.contains(""+ch)){
+            }
+            if (result.contains("" + ch)) {
                 continue;
-        }
-          result+=ch+""+count;
+            }
+            result += ch + "" + count;
         }
         System.out.println(result);
     }
