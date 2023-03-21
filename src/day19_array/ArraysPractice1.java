1a51aeb0bb229b0cc211a3975f5658b159b11bb7
omeragir <omeragir08@gmail.com>
2023-03-21 11:06:48 +0100

    ArraysPractice1

---
+++ src/day19_array/ArraysPractice1.java
@@ -0,0 +1,30 @@
+package day19_array;
+
+import java.util.Arrays;
+
+public class ArraysPractice1 {
+
+    public static void main(String[] args) {
+
+        String [] studentsName=new String[5];
+        studentsName[0]="Mike";
+        studentsName[1]="John";
+        studentsName[2]="James";
+        studentsName[3]="Emily";
+        studentsName[4]="Hans";
+
+        System.out.println(Arrays.toString(studentsName));
+
+        System.out.println("----------------------------------");
+
+        for (int i = studentsName.length-1; i >=0 ; i--) {
+            System.out.println(studentsName[i]);
+
+        }
+
+
+
+
+
+    }
+}
