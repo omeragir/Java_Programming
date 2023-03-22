dab0f6b196a784fa7b12301409b9ddb09f1389ad
omeragir <omeragir08@gmail.com>
2023-03-22 09:32:57 +0100

    ReverseArrays

--- src/day20_forEach/ReverseArrays.java
+++ src/day20_forEach/ReverseArrays.java
@@ -1,8 +1,20 @@
 package day20_forEach;
 
+import java.util.Arrays;
+
 public class ReverseArrays {
     public static void main(String[] args) {
 
+        int[] array = {1, 2, 3, 4, 5};
+
+        int[] reverse = new int[array.length];
+
+        for (int i = array.length - 1, j = 0; i >= 0; i--, j++) {
+            reverse[j] = array[i];
+
+
+        }
+        System.out.println(Arrays.toString(reverse));
     }
 }
 /*
