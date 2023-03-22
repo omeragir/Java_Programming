54ed02708463e4bd115fca7c40a42ba517bd49b4
omeragir <omeragir08@gmail.com>
2023-03-22 15:24:20 +0100

    ClassMates

---
+++ src/day20_forEach/ClassMates.java
@@ -0,0 +1,28 @@
+package day20_forEach;
+
+import java.util.Arrays;
+
+public class ClassMates {
+    public static void main(String[] args) {
+
+        String [] classMatesNames={"johny Deep","Merlyn Moore","Ahmet Ag","Mehmet Gel","Emily Emily","Chuck Chuck"};
+        System.out.println(Arrays.toString(classMatesNames));
+
+
+        System.out.println("--------------------------------------------");
+
+        for (int i = 0; i < classMatesNames.length; i++) {
+            System.out.println(classMatesNames[i]);
+
+        }
+
+
+
+
+
+    }
+}
+/*
+create an array named classmates, and store 10 of your classmates' full names
+            print the initials of each classmate's in separate lines
+ */
