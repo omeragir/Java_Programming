9e36ecc3b5c0e1ec06f1c29597c4873920ec21ce
omeragir <omeragir08@gmail.com>
2023-03-29 14:17:56 +0200

    ArrayList

---
+++ src/day23_ArrayList/ArrayArrayListConversion.java
@@ -0,0 +1,44 @@
+package day23_ArrayList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayArrayListConversion {
+    public static void main(String[] args) {
+
+        String[] arr={"A","B","C","D"};
+
+        ArrayList<String>list=new ArrayList<>(Arrays.asList(arr));
+
+        System.out.println(list);
+
+        System.out.println("-----------------");
+
+        ArrayList<String> list2=new ArrayList<>(Arrays.asList("Java","Python","C#"));
+
+        String[] languages=list2.toArray(new String[list2.size()]);//i give 0 or list.size()
+
+        System.out.println(Arrays.toString(languages));
+
+        System.out.println("--------------------------------");
+        ArrayList<Integer> nums=new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8));
+        Integer[] n=nums.toArray(new Integer[0]);
+        System.out.println(Arrays.toString(n));
+        System.out.println("-----------------------------------");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
