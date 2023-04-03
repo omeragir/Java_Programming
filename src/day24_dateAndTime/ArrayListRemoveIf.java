e2118bd58a38fe85816a7651982f619e0f6d451a
omeragir <omeragir08@gmail.com>
2023-04-03 10:37:57 +0200

    ArrayList

--- src/day24_dateAndTime/ArrayListRemoveIf.java
+++ src/day24_dateAndTime/ArrayListRemoveIf.java
@@ -1,5 +1,7 @@
 package day24_dateAndTime;
 
+import day17_customClass.Employee;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -21,8 +23,31 @@ public class ArrayListRemoveIf {
 
         System.out.println(list2);
 
-        list2.removeIf(p -> p < 4);
+        list2.removeIf(p -> p < 4);  //removeIf method
         System.out.println(list2);
+        System.out.println("----------------------------------");
+
+
+        ArrayList<String> names = new ArrayList<>();    //String removeIf method
+        names.addAll(Arrays.asList("Java", "Python", "C#", "Java", "Ruby"));
+        names.removeIf(p -> p.startsWith("J"));
+        System.out.println(names);
+        System.out.println("----------------------------------------");
+
+        ArrayList<Employee> employees = new ArrayList<>();
+        employees.addAll(Arrays.asList(new Employee(), new Employee(), new Employee(), new Employee(), new Employee()));
+
+        employees.get(0).setInfo("Josh", 34, 'M', "Java Developer", 100000, "A01");
+        employees.get(1).setInfo("Emily", 40, 'F', "SDETs", 90000, "A02");
+        employees.get(2).setInfo("Conor", 38, 'M', "Project Manager", 130000, "A03");
+        employees.get(3).setInfo("Bella", 29, 'F', "Java Developer", 95000, "A04");
+        employees.get(4).setInfo("Jimmy", 54, 'M', "Data Analyst", 105000, "A05");
+
+
+        employees.removeIf(p -> p.salary > 100_000);
+        for (Employee each : employees) {
+            System.out.println(each.name + " : " + each.salary);
+        }
 
     }
 }
