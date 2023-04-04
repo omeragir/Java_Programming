5d7d417aff2555a5626b1d53d6ab1826774aa2f2
omeragir <omeragir08@gmail.com>
2023-04-04 18:07:23 +0200

    PizzaTestObject

--- src/day25_constructor/PizzaTestObject.java
+++ src/day25_constructor/PizzaTestObject.java
@@ -4,8 +4,12 @@ public class PizzaTestObject {
     public static void main(String[] args) {
 
         Pizza pizza=new Pizza('L',2,5);
+        Pizza pizza1=new Pizza('S',3,1);
+        Pizza pizza2=new Pizza('M',2,2);
 
         System.out.println(pizza);
+        System.out.println(pizza1);
+        System.out.println(pizza2);
 
     }
 }
