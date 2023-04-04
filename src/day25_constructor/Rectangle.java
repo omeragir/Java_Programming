b8e642eec9187e1f192f3efd2608ca0d8d9b84f1
omeragir <omeragir08@gmail.com>
2023-04-04 15:37:28 +0200

    Rectangle

---
+++ src/day25_constructor/Rectangle.java
@@ -0,0 +1,23 @@
+package day25_constructor;
+
+public class Rectangle {
+
+    public  double length, width;
+
+    public void setInfo(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double area(){
+        return length*width;
+    }
+
+
+    public String toString() {
+        return "Rectangle{" +
+                "length=" + length +
+                ", width=" + width +
+                '}';
+    }
+}
