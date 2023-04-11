7ced033a13ccdba39f70c7a0d6039092f812fd08
omeragir <omeragir08@gmail.com>
2023-04-11 09:41:40 +0200

    Java Programming Day 27

---
+++ src/utilities/Test2.java
@@ -0,0 +1,23 @@
+package utilities;
+
+//import day27_accessModifiers.Data;
+
+import static day27_accessModifiers.Data.d;
+
+import static day27_accessModifiers.Data.method3;
+
+import static day27_accessModifiers.Data.*;
+
+public class Test2 {
+    public static void main(String[] args) {
+        //System.out.println(Data.d);
+
+        System.out.println(d);
+
+        method3();
+
+        System.out.println(e);
+
+        method4();
+    }
+}
