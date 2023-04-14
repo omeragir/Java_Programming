b37b544e5180b9f2ea223b6652c7c4ea4f6a1568
omeragir <omeragir08@gmail.com>
2023-04-14 11:00:14 +0200

    Java Programming Day 29

--- src/day29_inheritance/animalTask/Zoo.java
+++ src/day29_inheritance/animalTask/Zoo.java
@@ -4,7 +4,44 @@ public class Zoo {
 
     public static void main(String[] args) {
 
-        Dog dog=new Dog();
+        Dog dog = new Dog();
+        dog.setInfo("Max", "Husky", 'm', 4, "Large", "White");
+
+        Cat cat = new Cat();
+        cat.setInfo("Felicia", "Stray", 'F', 3, "Medium", "Black");
+
+        Tiger tiger = new Tiger();
+        tiger.setInfo("Sher Khan", "Bengal", 'M', 5, "Large", "Orange");
+
+
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(tiger);
+        System.out.println("----------------------------------");
+
+        dog.eat();
+        dog.sleep();
+        dog.drink();
+
+        cat.eat();
+        cat.drink();
+        cat.sleep();
+
+        tiger.eat();
+        tiger.drink();
+        tiger.sleep();
+        System.out.println("-----------------");
+
+        dog.bark();
+        //cat.bark();
+
+
+        cat.meow();
+        //dog.meow();
+
+        tiger.hunt();
+        //dog.hunt();
+        System.out.println("-----------------");
 
 
     }
