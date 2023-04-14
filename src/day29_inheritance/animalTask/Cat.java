4c819f5b1d8bab6f63046c4af7f656143c49f226
omeragir <omeragir08@gmail.com>
2023-04-14 10:25:54 +0200

    Java Programming Day 29

---
+++ src/day29_inheritance/animalTask/Cat.java
@@ -0,0 +1,15 @@
+package day29_inheritance.animalTask;
+
+public class Cat extends Animal{
+
+
+
+
+    public void meow(){
+        System.out.println(name+ " is meowing");
+    }
+    public void scratch(){
+        System.out.println(name+" is scratching");
+    }
+
+}
