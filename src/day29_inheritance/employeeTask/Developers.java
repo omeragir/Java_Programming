2ac4f77a2b41fafd34472f8259cbff04bcd20b4b
omeragir <omeragir08@gmail.com>
2023-04-15 11:27:58 +0200

    Java Programming Day 29

--- src/day29_inheritance/employeeTask/Developers.java
+++ src/day29_inheritance/employeeTask/Developers.java
@@ -25,3 +25,40 @@ public class Developers extends Employee {
 
     }
 }
+/*   #2 solution
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Developer extends Employee{
+
+    private String programmingLanguage;
+
+    public void setInfo(String name, char gender, int age, int id, String jobTitle, double salary, String programmingLanguage) {
+        setInfo(name, gender, age, id, jobTitle, salary);
+        setProgrammingLanguage(programmingLanguage);
+    }
+
+    public String getProgrammingLanguage() {
+        return programmingLanguage;
+    }
+
+    public void setProgrammingLanguage(String programmingLanguage) {
+        ArrayList<String> list = new ArrayList<>();
+        list.addAll(Arrays.asList("Java", "JavaScript", "Python", "Ruby", "C#", "C++", "Swift"));
+
+        if (!list.contains(programmingLanguage)) {
+            System.err.println("Invalid programming language: "+programmingLanguage);
+            System.exit(1);
+        }
+        this.programmingLanguage = programmingLanguage;
+    }
+
+
+
+    public void coding(){
+        System.out.println(getJobTitle()+" "+getName()+" is coding in "+programmingLanguage);
+    }
+
+
+}
+ */
