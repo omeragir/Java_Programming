c2cdd24aead8fc56c478f3fefc196bb121c422c6
omeragir <omeragir08@gmail.com>
2023-04-17 09:25:57 +0200

    Java Programming Day 31

---
+++ src/day31_methodOverriding/ProtectedAccessModifier.java
@@ -0,0 +1,8 @@
+package day31_methodOverriding;
+
+public class ProtectedAccessModifier {
+    protected static int a=100;
+    protected static void method1(){
+
+    }
+}
---
+++ src/utilities/Test3.java
@@ -0,0 +1,12 @@
+package utilities;
+
+import day31_methodOverriding.ProtectedAccessModifier;
+
+public class Test3 extends ProtectedAccessModifier {
+    public static void main(String[] args) {
+
+
+        System.out.println(ProtectedAccessModifier.a);
+        ProtectedAccessModifier.method1();
+    }
+}
