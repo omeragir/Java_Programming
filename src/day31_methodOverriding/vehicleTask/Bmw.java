5dad9c3bd0cecb53c236ad3bdc966b00eb01d1a1
omeragir <omeragir08@gmail.com>
2023-04-17 09:10:40 +0200

    Java Programming Day 31

---
+++ src/day31_methodOverriding/vehicleTask/Bmw.java
@@ -0,0 +1,10 @@
+package day31_methodOverriding.vehicleTask;
+
+public class Bmw extends Car{
+
+    public Bmw(String brand, String model, String color, int year, double price) {
+        super(brand, model, color, year, price);
+    }
+
+
+}
---
+++ src/day31_methodOverriding/vehicleTask/Yacht.java
@@ -0,0 +1,10 @@
+package day31_methodOverriding.vehicleTask;
+
+public class Yacht extends Boat{
+
+    public Yacht(String brand, String model, String color, int year, double price) {
+        super(brand, model, color, year, price);
+    }
+
+
+}
