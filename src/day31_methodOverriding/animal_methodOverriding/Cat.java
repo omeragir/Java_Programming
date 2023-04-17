a5c47c8161c91ab8776f162ff2626b794bedd36a
omeragir <omeragir08@gmail.com>
2023-04-17 10:09:31 +0200

    Java Programming Day 31

--- src/day31_methodOverriding/animal_methodOverriding/Cat.java
+++ src/day31_methodOverriding/animal_methodOverriding/Cat.java
@@ -1,18 +1,22 @@
 package day31_methodOverriding.animal_methodOverriding;
 
-public class Cat extends Animal{
+public class Cat extends Animal {
 
     public Cat(String name, String breed, char gender, int age, String size, String color) {
         super(name, breed, gender, age, size, color);
     }
 
 
-    public void eat(){
-        System.out.println("Cat "+getName()+" is eating cat food");
+    public void eat() {
+        System.out.println("Cat " + getName() + " is eating cat food");
     }
 
 
     public void sleep() {
-        System.out.println("Cat "+getName()+" sleep 12 hours in a day");
+        System.out.println("Cat " + getName() + " sleep 12 hours in a day");
+    }
+
+    public void scratch() {
+        System.out.println("Cat " + getName() + " is scratching");
     }
 }
