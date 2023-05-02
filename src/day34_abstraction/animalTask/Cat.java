8451656de1d2412e7c0b77fcd0350668f54617b1
omeragir <omeragir08@gmail.com>
2023-05-02 14:07:02 +0200

    Java Programming Day 34

--- src/day34_abstraction/animalTask/Cat.java
+++ src/day34_abstraction/animalTask/Cat.java
@@ -1,6 +1,6 @@
 package day34_abstraction.animalTask;
 
-public class Cat extends Animal{
+public class Cat extends Animal implements Playable{
 
     public Cat(String name, String breed, char gender, int age, String size, String color) {
         super(name, breed, gender, age, size, color);
@@ -9,5 +9,10 @@ public class Cat extends Animal{
     @Override
     public void eat() {
         System.out.println("Cat  "+getName()+" is eating cat food");
+    }
+
+    @Override
+    public void playable() {
+        System.out.println("Cat is playing with people");
     }
 }
--- src/day34_abstraction/animalTask/Dolphin.java
+++ src/day34_abstraction/animalTask/Dolphin.java
@@ -1,6 +1,6 @@
 package day34_abstraction.animalTask;
 
-public class Dolphin extends Animal{
+public class Dolphin extends Animal implements Playable{
     public Dolphin(String name, String breed, char gender, int age, String size, String color) {
         super(name, breed, gender, age, size, color);
     }
@@ -8,5 +8,10 @@ public class Dolphin extends Animal{
     @Override
     public void eat() {
         System.out.println("Dolphin "+getName()+" is eating fish");
+    }
+
+    @Override
+    public void playable() {
+        System.out.println(getName()+" is playable animals");
     }
 }
