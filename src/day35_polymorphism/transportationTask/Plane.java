0e56582cf4e1d135ea51c41134438043ae2f6dd3
omeragir <omeragir08@gmail.com>
2023-05-03 09:06:08 +0200

    Java Programming Day 35

--- src/day35_polymorphism/transportationTask/Plane.java
+++ src/day35_polymorphism/transportationTask/Plane.java
@@ -2,28 +2,15 @@ package day35_polymorphism.transportationTask;
 
 import day34_abstraction.animalTask.Flyable;
 
-public class Plane extends Transportation implements Flyable {
+public abstract class Plane extends Transportation implements Flyable {
 
     public Plane(String make, String model, String color, int year, double price) {
         super(make, model, color, year, price);
     }
 
 
-    public void fly() {
-        System.out.println("Plane can fly");
-    }
-
-
-    public void transportPeople() {
-        System.out.println("Plane transport a lot of people");
-    }
-
-    public void start() {
-        System.out.println("Plane has different start mode");
-    }
-
     public void land(){
-        System.out.println("");
+        System.out.println("Plane "+getMake()+" "+getModel()+" is landing" );
     }
 
 }
