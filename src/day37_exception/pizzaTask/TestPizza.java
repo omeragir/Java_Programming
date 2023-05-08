5f619cab76622679f6f462dab2d492ff92b62b16
omeragir <omeragir08@gmail.com>
2023-05-08 08:58:34 +0200

    Java Programming 37

--- src/day37_exception/pizzaTask/TestPizza.java
+++ src/day37_exception/pizzaTask/TestPizza.java
@@ -1,18 +1,30 @@
 package day37_exception.pizzaTask;
 
 import day18_garbageCollection.TestPizzaObject;
+import day36_polymorphismContinue.Circle;
 
 public class TestPizza {
     public static void main(String[] args) {
 
-        Pizza pizza=new Pizza('S',2,3);
-        Pizza pizza1=new Pizza('S',2,3);
-        System.out.println(pizza1==pizza);  //false
+        Pizza pizza = new Pizza('S', 2, 3);
+        Pizza pizza1 = new Pizza('S', 2, 3);
+        System.out.println(pizza1 == pizza);  //false
 
         System.out.println(pizza.equals(pizza1));//true
 
-        System.out.println(pizza.equals("Pizza"));//Invalid object
+        //System.out.println(pizza.equals("Pizza"));//Invalid object
 
+        Object obj = new Pizza('S', 2, 3);//Upcasting
+
+        boolean r = obj.equals(pizza1);
+        System.out.println(r);
+
+        double total = ((Pizza) obj).calcCost();
+
+        System.out.println(total);
+
+        //   double area= ((Circle)obj).area();
+        //  System.out.println(area);
 
     }
 }
