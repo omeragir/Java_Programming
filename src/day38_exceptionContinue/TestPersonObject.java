253248c4719e59f5d5f10a8d6e0f4b1b566bd518
omeragir <omeragir08@gmail.com>
2023-05-09 10:28:48 +0200

    Java Programming 38

---
+++ src/day38_exceptionContinue/TestPersonObject.java
@@ -0,0 +1,17 @@
+package day38_exceptionContinue;
+
+public class TestPersonObject {
+    public static void main(String[] args) {
+
+        Person person1=new Person("Daniel",32,'M');
+
+        System.out.println(person1);
+try {
+    person1.setAge(-12);
+}catch (RuntimeException e){
+    e.printStackTrace();
+}
+
+        System.out.println(person1);
+    }
+}
