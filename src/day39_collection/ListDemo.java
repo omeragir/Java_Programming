0b1eb6f7dd4f089d1b7a45792e5ba2b7d76b47cf
omeragir <omeragir08@gmail.com>
2023-05-15 10:12:12 +0200

    Java Programming 39

---
+++ src/day39_collection/ListDemo.java
@@ -0,0 +1,39 @@
+package day39_collection;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListDemo {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        arrayList.add(100);
+        arrayList.add(200);
+        arrayList.add(300);
+
+        arrayList.remove(0);
+
+        arrayList.get(0);
+
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        linkedList.add(100);
+        linkedList.add(200);
+        linkedList.add(300);
+
+        linkedList.remove(0);
+
+        linkedList.get(0);
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/day39_collection/multiThread/ThreadHelloWorld.java
@@ -0,0 +1,22 @@
+package day39_collection.multiThread;
+
+public class ThreadHelloWorld extends Thread {
+
+    int threadNumber;
+
+    public ThreadHelloWorld(int threadNumber) {
+        this.threadNumber = threadNumber;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i < 6; i++) {
+            System.out.println(i+" Hello World Thread"+threadNumber);
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
