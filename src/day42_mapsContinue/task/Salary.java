7672f141a47e796e03f83c14514534bbcbc54c1b
omeragir <omeragir08@gmail.com>
2023-05-25 08:23:24 +0200

    Java Programming 42

--- src/day42_mapsContinue/task/Salary.java
+++ src/day42_mapsContinue/task/Salary.java
@@ -17,13 +17,67 @@ public class Salary {
         map.put("Anderson", 125000);
         map.put("Steven", 135000);
 
+        //2.1 who has the maximum and minimum salary?
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        String maxName = null;
+        String minName = null;
+
+        for (String key : map.keySet()) {
+            int value = map.get(key);
+            if (value > max) {
+                max = value;
+                maxName = key;
+            }
+
+            if (value < min) {
+                min = value;
+                minName = key;
+            }
+        }
+
+        System.out.println("Max: " + maxName + " - " + max);
+        System.out.println("Min: " + minName + " - " + min);
+
+        //2.2 how many employees has the salary between 120k ~ 150K?
+        System.out.println();
+
+        int count = 0;
+        for (int value : map.values()) {
+            if (value >= 120_000 && value <= 150_000) {
+                count++;
+            }
+        }
+
+        System.out.println("Between 120K & 150K " + count);
+
+        //2.3 display the names of the employees who are making less than 118k?
+        System.out.println();
+
+        for (String key : map.keySet()) {
+            if (map.get(key) <= 118_000) {
+                System.out.println(key);
+            }
+        }
+
+        // 2.4 increase the salary employee by 10K if the current salary of employee is less than 120K
+        System.out.println();
+        for (String key : map.keySet()) {
+            if (map.get(key) <= 120_000) {
+                map.put(key, map.get(key) + 10_000);
+            }
+        }
+
         System.out.println(map);
-
-
-
 
     }
 }
+
+
+
+
+
 /*
 2.1 who has the maximum and minimum salary?
 
